a24920e8b4fe5381aa1438957224b20e0dceb0f9
Vueaaa <1377837526@qq.com>
2024-09-01 20:25:42 +0800

    main

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -6,30 +6,50 @@ import Tabs from "./components/Tabs.vue"
 
 <template>
   <div class="wrapper">
-        <FHeader></FHeader>
-        <FMenu></FMenu>
-        <div class="content-box">
-        <Tabs></Tabs>
+    <FHeader></FHeader>
+    <FMenu></FMenu>
+    <div class="content-box">
+        <div class="content">
+          <router-view v-slot="{ Component }">
+            <transition name="move" mode="out-in">
+              <!-- <keep-alive :include="tabs.nameList"> -->
+                <component :is="Component"></component>
+              <!-- </keep-alive> -->
+            </transition>
+          </router-view>
         </div>
-  </div>
+    </div>
+    </div>
 </template>
 
 <style scoped>
-.wrapper{
+.wrapper {
   height: 100vh;
   overflow: hidden;
 }
-.content-box{
+
+.content-box {
   position: absolute;
-    left: 250px;
-    right: 0;
-    top: 70px;
-    bottom: 0;
-    padding-bottom: 30px;
-    /* 设置过渡效果，持续时间为0.3秒，过渡的时间函数是 ease-in-out（开始和结束慢，中间快） */
-    -webkit-transition: left 0.3s ease-in-out;
-    transition: left 0.3s ease-in-out;
-    background: #eef0fc;
-    /* overflow: hidden; */
+  left: 250px;
+  right: 0;
+  top: 70px;
+  bottom: 0;
+  padding-bottom: 30px;
+  /* 设置过渡效果，持续时间为0.3秒，过渡的时间函数是 ease-in-out（开始和结束慢，中间快） */
+  -webkit-transition: left 0.3s ease-in-out;
+  transition: left 0.3s ease-in-out;
+  background: #eef0fc;
+  /* overflow: hidden; */
+}
+.content {
+    width: auto;
+    height: 100%;
+    padding: 20px;
+    overflow-y: scroll;
+    box-sizing: border-box;
+}
+
+.content::-webkit-scrollbar {
+    width: 0;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,27 @@
-import {createRouter, createWebHashHistory} from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 let routes = [
+  {
+    path: "/",
+    redirect: "/dashboard",
+  },
   {
     path: "/",
     name: "admin",
     //使用import可以路由懒加载，如果不使用，太多组件一起加载会造成白屏
     component: () => import("@/layouts/admin.vue"),
-    meta: {
-      keepAlive: false,
-      title: "首页",
-    },
+    children: [
+      {
+        path: "/dashboard",
+        name: "dashboard",
+        meta: {
+          title: "系统首页",
+          KeepAlive: false,
+        },
+        component: () =>
+          import(/* webpackChunkName: "dashboard" */ "@/views/dashboard/index.vue"),
+      },
+    ],
   },
   {
     path: "/login",
@@ -21,14 +33,20 @@ let routes = [
       title: "登录页",
     },
   },
-
+  // {
+  //   path: '/dashboard',
+  //   name: 'dashboard',
+  //   //使用import可以路由懒加载，如果不使用，太多组件一起加载会造成白屏
+  //   component: () => import("@/views/dashboard/index.vue"),
+  //   meta: {
+  //     keepAlive: false,
+  //     title: '系统首页',
+  //   },
+  // }
 ];
-
 
 // 路由
 export const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
-
-
